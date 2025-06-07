965ed456b952d51e7b53aacc9f3fa882951b3f17
Santt4na <jhonnsantt4na@gmail.com>
2025-06-07 15:30:45 -0300

    feat: initial setup of Spring Boot project with dependencies (#1)

---
+++ src/main/java/com/santt4na/health_check/Startup.java
@@ -0,0 +1,13 @@
+package com.santt4na.health_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
